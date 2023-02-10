9175259b0f30e99fa3c5dc065a0d2eae254ddae8
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2023-02-10 00:27:58 -0800

    compatibility changes with SlyAPI

--- src/SlyYTAAPI/analytics.py
+++ src/SlyYTAAPI/analytics.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, asdict
 from datetime import date, timedelta
 import json, csv
-from typing import Any
+from enum import Enum
+from typing import Any, TypeVar
 from SlyAPI import *
 
 def makeFilters(filters: dict[str, Any]) -> str:
@@ -10,22 +11,29 @@ def makeFilters(filters: dict[str, Any]) -> str:
         out.append(F"{key}=={value}")
     return ';'.join(out)
 
-class Dimensions(EnumParam):
-    '''
-    From https://developers.google.com/youtube/analytics/dimensions
-    '''
+_T = TypeVar('_T')
+class _AddOperator_Set(set[_T]):
+    '''set with + operator. Workaround for backwards compatibility with
+    EnumParams (type removed from SlyAPI)'''
+    def __add__(self, other: _T|set[_T]) -> '_AddOperator_Set[_T]':
+        if isinstance(other, set):
+            return _AddOperator_Set((*self, *other))
+        return _AddOperator_Set((*self, other))
+
+class Dimensions(Enum):
+    'From https://developers.google.com/youtube/analytics/dimensions'
     Day             = 'day'
     Month           = 'month'
     #Week            = '7DayTotals' # deprecated, valid until April 15, 2020
     Country         = 'country'
     Video           = 'video'
     # ...
-    
 
-class Metrics(EnumParam):
-    '''
-    From https://developers.google.com/youtube/reporting#metrics.
-    '''
+    def __add__(self: 'Dimensions', other: 'Dimensions'|set['Dimensions']) -> _AddOperator_Set['Dimensions']:
+        return _AddOperator_Set((self,)) + other
+
+class Metrics(Enum):
+    'From https://developers.google.com/youtube/reporting#metrics'
     #Revenue         = 'estimated_partner_revenue' 
     Views           = 'views'
     #TrafficSource   = 'traffic_source_detail'
@@ -36,6 +44,9 @@ class Metrics(EnumParam):
     SubsGained      = 'subscribersGained'
     SubsLost        = 'subscribersLost'
     # ...
+
+    def __add__(self: 'Metrics', other: 'Metrics'|set['Metrics']) -> _AddOperator_Set['Metrics']:
+        return _AddOperator_Set((self,)) + other
 
 @dataclass
 class QueryResult:
@@ -82,27 +93,40 @@ class YouTubeAnalytics(WebAPI):
         auth.verify_scope(scope)
         self.channel_id = channel_id
 
-    async def video(self, video_id: str, since: date, metrics: Metrics, dims: Dimensions, end_date: date|None=None) -> QueryResult:
+    # Backwards compatibility with SlyAPI < 0.4.0
+    # Do nothing when awaited
+    def __await__(self):
+        async def delay(): return self
+        return delay().__await__()
+
+    async def video(self, video_id: str, since: date, metrics: Metrics|set[Metrics], 
+        dims: Dimensions|set[Dimensions], end_date: date|None=None) -> QueryResult:
         return await self.query(since, metrics, dims, end_date, {'video': video_id})
 
-    async def query(self, since: date, metrics: Metrics, dims: Dimensions, end_date: date|None=None, filters:dict[str, Any]|None=None) -> QueryResult:
+    async def query(self, since: date, metrics: Metrics|set[Metrics],
+        dims: Dimensions|set[Dimensions], end_date: date|None=None, filters:dict[str, Any]|None=None) -> QueryResult:
         if end_date is None:
             end_date = date.today()
         if dims == Dimensions.Month: # month requires end date as first day of next month
             end_date = end_date - timedelta(days=end_date.day-1)
+        # normalize to sets
+        if isinstance(metrics, Metrics): metrics = {metrics}
+        if isinstance(dims, Dimensions): dims = {dims}
+
         result = await self._reports_query(since, end_date , metrics, dims, filters)
         return QueryResult(**result)
 
-    async def _reports_query(self, start_date: date, end_date: date, metrics: Metrics, dims: Dimensions | None = None, filters: dict[str, Any]|None=None) -> dict[str, Any]:
+    async def _reports_query(self, start_date: date, end_date: date, metrics: set[Metrics], dims: set[Dimensions], filters: dict[str, Any]|None=None) -> dict[str, Any]:
         params = {
             'startDate': start_date.isoformat(),
             'endDate': end_date.isoformat(),
-            'ids': F"channel=={self.channel_id}"
-        } | metrics.to_dict()
+            'ids': F"channel=={self.channel_id}",
+            'metrics': ','.join(metric.value for metric in metrics)
+        }
         if filters:
             params['filters'] = makeFilters(filters)
         if dims:
-            params |= dims.to_dict()
+            params['dimensions'] = ','.join(dim.value for dim in dims)
 
         return await self.get_json(F"/reports", params)
     
